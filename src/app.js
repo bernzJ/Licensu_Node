0b26c351a1c00dd6c86f0588b1d8d0c3548ee5ff
benJ <ben-0_0-@hotmail.com>
2018-02-27 14:33:49 -0500

    init

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import serialNumber from "serialNumber";
+import https from "https";
+import fs from "fs";
+
+const answers = [
+  { KEY_UPDATE: 232 },
+  { NOT_FOUD: 321 },
+  { EXPIRED_LICENSE: 144 },
+  { BANNED: 242 },
+  { UPDATE: 69 },
+  { PLUGIN_LIST: 3141 },
+  { PLUGIN_DATA: 3562 },
+  { PLUGIN_NO_ACCESS: 48879 },
+  { DATA_EOF: 195894762 },
+  { DONE_PROCESSING: 57005 },
+  { INTERNAL_ERROR: 194 }
+];
+
+const trustedCa = ["../certs/ca.crt", "../certs/clientcert.p12"];
+
+https.globalAgent.options.ca = [];
+for (const ca of trustedCa) {
+  https.globalAgent.options.ca.push(fs.readFileSync(ca));
+}
